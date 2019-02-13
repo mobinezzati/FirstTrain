a2f7f0e8c99f69210128747e3d0a657eeb36526b
Mobin Ezzati <shemob11@yahoo.com>
2019-02-13 18:54:36 +0330

    Initial Commit

---
+++ FirstTrain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstTrain
+//
+//  Created by mobin on 2/13/19.
+//  Copyright © 2019 mobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
